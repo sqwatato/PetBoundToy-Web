e4d9e2fa17421627d584b45e5f68e51a460a16ac
Mahasvin24 <mahasvin.manikandan@gmail.com>
2023-08-20 19:30:56 -0700

    Fixed Card Issues + Added Styling

--- components/PetCard.tsx
+++ components/PetCard.tsx
@@ -28,8 +28,10 @@ const PetCard = ({ type, name, species, breed, age, gender, size, description, s
 
   return (
     <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
-      <div className="max-w-sm rounded-lg overflow-hidden shadow-lg">
-        <img src={image} alt="hero image" />
+      <div className="max-w-sm w-full h-full rounded-lg overflow-hidden shadow-lg border border-gray-300 border-opacity-50">
+        <div className="relative h-40 bg-gray-100"> {/**Use this to adjust image height*/}
+          <img className="object-contain w-full h-full" src={image} alt="hero image" />
+        </div>
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2 text-black">{name}</div>
           <p className="text-gray-700 text-base">
@@ -37,8 +39,8 @@ const PetCard = ({ type, name, species, breed, age, gender, size, description, s
           </p>
         </div>
         <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{type == 'A' ? 'Adoption' : 'Foster' }</span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{age} years old</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{type === 'A' ? 'Adoption' : 'Foster'}</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{age} y/o</span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{breed}</span>
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{gender}</span>
         </div>
